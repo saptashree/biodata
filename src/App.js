c9c0519dc6b4a3e5d33a07020f30b9a36c8330d0
saptashree <saptashree.vb17@gmail.com>
2021-09-01 13:51:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+ 
+import React from 'react';
+import Home from './Biodata/ Home';
+
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Biodata/ Home.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import { Container, Row, Table } from 'reactstrap';
+
+function Home() {
+    return (
+        <div>
+            <Container>
+              <Row>
+                <Table>
+                  <thead >
+                    <th className={"w-50"}>Personal Details:</th>
+                    
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        Name
+                      </td>
+                      <td>Shreyas R Halageri</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Date of Birth
+                      </td>
+                      <td>9th Jan 1991</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Time of Birth
+                      </td>
+                      <td>6.00 PM</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Height
+                      </td>
+                      <td>5feet 8 inches</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Native place
+                      </td>
+                      <td>Hirekerur, Ingalagondi</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Nakshatra
+                      </td>
+                      <td>Swathi</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Rashi
+                      </td>
+                      <td>Tula</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Caste
+                      </td>
+                      <td>Hindu Lingayat Sadaru</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Bedagu
+                      </td>
+                      <td>M lru</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Qualification
+                      </td>
+                      <td>BE, PG in Computer Science</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Company
+                      </td>
+                      <td>Software Architect @ FoundationAI</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Previous Organization
+                      </td>
+                      <td>Infosys, Practo</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Salary
+                      </td>
+                      <td>24 Lakhs per annum</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Father
+                      </td>
+                      <td>Revanasiddappa S Halageri ( retired KFCSC), currently working as advocate</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Mother
+                      </td>
+                      <td>Vijaya R H (Home maker)</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Elder Brother
+                      </td>
+                      <td>Tejas R Halageri (Married)</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        God
+                      </td>
+                      <td>Hala Siddeshwara (Ujjani Siddeshwara ) </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Elder Brother
+                      </td>
+                      <td>Tejas R Halageri (Married)</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Place
+                      </td>
+                      <td>Bengaluru</td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        Mobile No
+                      </td>
+                      <td>+919535055255, +919980165571</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Home;
